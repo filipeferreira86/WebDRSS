a18300dc258b5ef379a4659d675238d0493565f9
filipe.jesus <filipe.jesus@gx2-13.gx2.local>
2019-01-16 17:22:41 -0200

    Teste final tentativa 002

--- src/test/java/func/PrtSrc.java
+++ src/test/java/func/PrtSrc.java
@@ -1,19 +1,16 @@
 package func;
 
-import java.util.concurrent.TimeUnit;
-
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 
-
 public class PrtSrc {
 
-	public static byte[] capturar(WebDriver driver) {
-		driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);
-			TakesScreenshot ts = (TakesScreenshot)driver;
-			byte[] source = ts.getScreenshotAs(OutputType.BYTES);
-			
+	public static byte[] capturar(WebDriver driver) throws InterruptedException {
+		Thread.sleep(1500);
+		TakesScreenshot ts = (TakesScreenshot) driver;
+		byte[] source = ts.getScreenshotAs(OutputType.BYTES);
+
 		return source;
 	}
 
